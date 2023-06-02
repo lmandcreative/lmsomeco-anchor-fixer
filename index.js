6a7f2630733d65dd28c90abce16f5b0f4575047b
Niko Peltoniemi <niko.peltoniemi@lmand.fi>
2023-06-02 08:50:29 +0300

    Add logging

--- index.js
+++ index.js
@@ -2,18 +2,36 @@ const anchorFixer = {
   install(Vue, overrideOptions) {
     let options = {
       router: null,
+      debug: false,
       ...overrideOptions,
     };
+
+    if (window.anchordebug) {
+      options.debug = true;
+    }
 
     if (!options.router) {
       throw new Error("router option is required");
     }
 
     let router = options.router;
+    if (options.debug) {
+      console.group("Anchor Fixer");
+    }
     router.beforeEach((to, from, next) => {
+      if (options.debug) {
+        console.log("beforeEach", to, from);
+      }
       if (router.hasRoute(to.name)) {
+        if (options.debug) {
+          console.log("hasRoute", to.name);
+        }
         next();
         if (to.hash) {
+          if (options.debug) {
+            console.log("hasHash", to.hash);
+            console.log(document.querySelector(to.hash).offsetTop);
+          }
           window.scrollTo(0, document.querySelector(to.hash).offsetTop);
         }
         return;
@@ -24,6 +42,9 @@ const anchorFixer = {
         window.scrollTo(0, document.querySelector("#" + id).offsetTop);
       }
     });
+    if (options.debug) {
+      console.groupEnd();
+    }
   },
 };
 
